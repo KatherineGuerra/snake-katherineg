b21342a23cc626fb9c019fbc2c6c805393fe8dd3
katherine guerra <guerrakn@.gmail>
2014-10-03 11:27:00 -0700

    commit

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -1,2 +1,19 @@
+var snake;
 
+var context;
+var screenwidth;
+var screenheight;
+
+function gameIntialize() {
+  var canvas = document.getElementById("game-screen");
+  context = canvas
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
 
