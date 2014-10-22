8a9000d8b76431cdca31a95534e51e0889529587
katherine guerra <guerrakn@.gmail>
2014-10-22 11:30:18 -0700

    commit

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -92,6 +92,13 @@ function snakeUpdate() {
         snakeHeadX++;
     }
     
+    if(snakeDirection == "up") {
+        snakeHeadY++;
+    }
+    else if (snakeDirection == "left") {
+        snakeHeadX++;
+    }
+    
     snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
@@ -114,15 +121,15 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "white";
-    context.fillRect (food.x, food.y, snakeSize, snakeSize);
+    context.fillRect (food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
 
 /*-----------------------------------------------------------------------------
@@ -133,8 +140,20 @@ function setFoodPosition() {
 function keyboardHandler(event) {
     console.log(event);
     
-    if(event.keyCode == "39") {
+    if(event.keyCode == "39" && snakeDirection != "left") {
         snakeDirection = "right"
     } 
     
+    else if(event.keyCode == "40" && snakeDirection != "up") {
+        snakeDirection = "down";
+    }
+    
+    
+    if(event.keyCode == "38" && snakeDirection != "down") {
+        snakeDirection = "up"
+    }
+    
+    else if(event.keyCode == "37" && snakeDirection != "right") {
+        snakeDirection = "left";
+    }
 }
