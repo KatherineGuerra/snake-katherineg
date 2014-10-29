7c0c684226c851cc063ddcb518a12a482606fe41
katherine guerra <guerrakn@.gmail>
2014-10-29 11:27:48 -0700

    commit

--- public_html/JS/Snake.js
+++ public_html/JS/Snake.js
@@ -16,6 +16,9 @@ var screenHeight;
 
 var gameState;
 var gameOverMenu;
+var restartButton;
+var playHUD;
+var scoreboard;
 
 /*----------------------------------------------------------------------------
  * Executing Game Code
@@ -47,11 +50,18 @@ function gameInitialize() {
     gameOverMenu = document.getElementById("gameOver");
     centerMenuPosition(gameOverMenu);
     
+    restartButton = document.getElementById("restartButton");
+    restartButton.addEventListener("click", gameRestart);
+    
+    playHUD = document.getElementById("playHUD");
+    scoreboard = document.getElementById("scoreboard");
+    
     setState("PLAY");
 }
 
 function gameLoop() {
     gameDraw();
+    drawScoreboard();
     if (gameState == "PLAY") {
         snakeUpdate();
         snakeDraw();
@@ -63,6 +73,14 @@ function gameDraw() {
     context.fillStyle = "rgb(77,82,81)";
     context.fillRect(0, 0, screenWidth, screenHeight);
 
+}
+
+function gameRestart() {
+    gameInitialize();
+    snakeInitialize();
+    foodInitialize();
+    hideMenu(gameOverMenu);
+    setState("PLAY");
 }
 
 /*----------------------------------------------------------------------------
@@ -210,13 +228,24 @@ function displayMenu(menu){
     menu.style.visibility = "visible";
 }
 
+function hideMenu(menu){
+    menu.style.visibility = "hidden";
+}
+
 function showMenu(state) {
     if (state =="GAME OVER") {
         displayMenu(gameOverMenu);
     }
+    else if (state =="PLAY") {
+        displayMenu(playHUD);
+    }
 }
 
 function centerMenuPosition(menu) {
-    menu.style.top = (screenHeight / 2) + "px";
-    menu.style.left = (screenWidth / 2) + "px";
+    menu.style.top = (screenHeight / 2) - (menu.offsetHeight / 2) + "px";
+    menu.style.left = (screenWidth / 2) - (menu.offsetWidth / 2) + "px";
+}
+
+function drawScoreboard() {
+    scoreboard.innerHTML = "length: " + snakeLength;
 }
